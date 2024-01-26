d695988fe9fec1d68f9670302a27dfc68f293e78
Yohann-cell <58658875+Yohann-cell@users.noreply.github.com>
2024-01-26 11:11:51 +0100

    Update interface.py

--- interface.py
+++ interface.py
@@ -4,6 +4,7 @@ import yfinance as yf
 from streamlit_gsheets import GSheetsConnection
 
 conn = st.experimental_connection("gsheets", type=GSheetsConnection)
+df = conn.read(worksheet="Feuille 1")
 
 formulaire = st.form(key = 'formulaire1')
 secteur = formulaire.multiselect('Sector :',['Basic Materials', 'Communication Services', 'Consumer Cyclical', 'Consumer Defensive', 'Energy', 'Financial Services', 'Healthcare', 'Industrials','Real Estate','Technology','Utilities'])
